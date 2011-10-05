19c2f47851da9122c4643631eb2f49c71c2f75ef
Michi Zahno <info@smartforce.ch>
2011-10-05 12:05:11 +0200

    New options, setting overlay position

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ You will want to include jQuery:
    	<script src="jquery.jPicDetail.min.js"></script>
    	<script>
       	$('.jPicDetail').jPicDetail( {
-			stickDurationAfter: 0 // will it stick for some time after you left the image with your mouse?
+			stickDurationAfter: 0, // will it stick for some time after you left the image with your mouse?
+			overlayColor: "#000",
+			overlayTransparency: 0.8
 		 });
     </script>
 
@@ -24,6 +26,14 @@ http://www.opensource.org/licenses/mit-license.php
 
 
 ## Changelog
+
+Version : 0.4
+Released: 05th Oct, 2011
+Notes:
+- Setting init position of overlay with jquery
+- Updated CSS
+
+
 Launch  : September 2011
 Version : 0.3
 Released: 27th Sep, 2011 and converted into a jQuery plugin
--- js/jPicDetail.jQuery.js
+++ js/jPicDetail.jQuery.js
@@ -25,73 +25,88 @@
  */
 
 (function($) {
-    $.fn.jPicDetail = function(options) {
-
-        var defaults = {
-            stickDurationAfter: 800
-        };
-        options = $.extend({}, defaults, options);
-
-
-        return this.each(function() {
-            obj = $(this);
-
-            // Fetch dimension of the image
-            function getDimension(ob) {
-                var dimensions = [];
-                dimensions.imgHeight = ob.find("img").outerHeight();
-                dimensions.imgWidth = ob.find("img").outerWidth(); // Not used yet
-                dimensions.spanHeight = ob.find("span").outerHeight();
-                return dimensions;
-            }
-
-            function setDimension(ob) {
-                // Set height an width of containing elements (li, for now)
-                $(ob).css({
-                    "height": getDimension(ob).imgHeight,
-                    "width": getDimension(ob).imgWidth
-                });
-                // Set width of overlay (span, for now)
-                $(ob.find("span")).css({
-                    "width": getDimension(ob).imgWidth
-                });
-            }
-
-            // Run if all pics are loaded
-            $(obj).find("img").load(function() {
-                var thisImage = $(this);
-
-                // Hide overlay content
-                thisImage.siblings("span").css({
-                    "opacity": 0
-                });
-
-                // Set dimension on container
-                $(obj).find("li").each(function(index) {
-                    var thisLi = $(this);
-                    setDimension(thisLi);
-                });
-            });
-
-            // do the fun stuff
-            // throw in some more effects later
-            $(obj).find("li").hover(function(event) {
-                $(this).find("span").animate({
-                    opacity: 0.75,
-                    top: getDimension($(this)).imgHeight - (getDimension($(this)).spanHeight)
-                }, 200, function() {
-                    // Animation complete.
-                });
-                event.stopPropagation(); // Stop the event bubbling up, no idea if needed twice
-            }, function(event) {
-                $(this).find("span").delay(options.stickDurationAfter).animate({
-                    opacity: 0,
-                    top: getDimension($(this)).imgHeight + (getDimension($(this)).spanHeight)
-                }, 300, function() {
-                    // Animation complete.
-                });
-                event.stopPropagation();
-            });
-        });
-    };
+	$.fn.jPicDetail = function(options) {
+		
+		var defaults = {
+			stickDurationAfter: 800,
+			overlayColor: "#000",
+			overlayTransparency: 0.8
+		};
+		options = $.extend({}, defaults, options);
+		
+		
+		return this.each(function() {
+			obj = $(this);
+			
+			// Fetch dimension of the image
+			function getDimension(ob) {
+				var dimensions = [];
+				dimensions.imgHeight = ob.find("img").outerHeight();
+				dimensions.imgWidth = ob.find("img").outerWidth(); // Not used yet
+				dimensions.spanHeight = ob.find("span").outerHeight();
+				return dimensions;
+			}
+			
+			function setDimension(ob) {
+				// Set height an width of containing elements (li, for now)
+				$(ob).css({
+					"height": getDimension(ob).imgHeight,
+					"width": getDimension(ob).imgWidth
+				});
+				// Set width of overlay (span, for now)
+				$(ob.find("span")).css({
+					"width": getDimension(ob).imgWidth
+				});
+			}
+			
+			jQuery.fn.setPosition = function() {
+			    $(this).css({
+					"top": getDimension($(this).parent()).imgHeight,
+					"left": 0
+				});
+			};
+			
+			// Run if all pics are loaded
+			$(obj).find("img").load(function() {
+				var thisImage = $(this);
+				
+				// Hide overlay content
+				thisImage.siblings("span").css({
+					"opacity": 0
+				});
+				
+				// Set dimension on container
+				$(obj).find("li").each(function(index) {
+					var thisLi = $(this);
+					setDimension(thisLi);
+				});
+				
+				// Set styles on overlay
+				$(obj).find("span").css({
+					"opacity" : 0,
+					"backgroundColor" : options.overlayColor
+				}).setPosition();
+			});
+			
+			// do the fun stuff
+			// throw in some more effects later
+			$(obj).find("li").hover(function(event) {
+				$(this).find("span").animate({
+					opacity: options.overlayTransparency,
+					top: getDimension($(this)).imgHeight - (getDimension($(this)).spanHeight)
+				}, 200, function() {
+					// Animation complete.
+				});
+				event.stopPropagation(); // Stop the event bubbling up, no idea if needed twice
+			}, function(event) {
+				$(this).find("span").delay(options.stickDurationAfter).animate({
+					opacity: 0,
+					top: getDimension($(this)).imgHeight + (getDimension($(this)).spanHeight)
+				}, 300, function() {
+					// Animation complete.
+				});
+				event.stopPropagation();
+			});
+		});
+	};
 })(jQuery);
