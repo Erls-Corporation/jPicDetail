ba5b491d46b14f10de406f9aac8bb86039cb35f3
Michi Zahno <info@smartforce.ch>
2011-11-01 14:28:24 +0100

    Update
    Description text is now taken from the data-descr attribute
    CSS positions are generated by jQuery, much less css...
    Bugfixes

--- js/jPicDetail.jQuery.js
+++ js/jPicDetail.jQuery.js
@@ -7,16 +7,18 @@
  * Licensed under MIT
  * http://www.opensource.org/licenses/mit-license.php
  *
- * Launch  : September 2011
- * Version : 0.4
- * Released: 10th Oct, 2011
+ * Launch  : November 2011
+ * Version : 0.5
+ * Released: 1th Nov, 2011
  *
  * 
  * Usage (with default values):
  * $('.jPicDetail').jPicDetail({
- *    stickDurationAfter: 800,
- *    overlayColor: "#000",
- *    overlayTransparency: 0.8
+ *      stickDurationAfter: 800,
+ *      overlayColor: "#000",
+ *      overlayTransparency: 0.8,
+ *      descriptionTextColor: "#FFF",
+ *      descriptionPadding: 0
  * });
  * 
  * 
@@ -26,96 +28,105 @@
  */
 
 (function($) {
-    $.fn.jPicDetail = function(options) {
-        
-        var defaults = {
-            stickDurationAfter: 800,
-            overlayColor: "#000",
-            overlayTransparency: 0.8
-        };
-        options = $.extend({}, defaults, options);
-        
-        
-        return this.each(function() {
-            obj = $(this);
-            
-            // Fetch dimension of the image
-            function getDimension(ob) {
-                var dimensions = [];
-                dimensions.imgHeight = ob.find("img").outerHeight();
-                dimensions.imgWidth = ob.find("img").outerWidth();
-                dimensions.spanHeight = ob.find("span").outerHeight();
-                return dimensions;
-            }
-            
-            function setDimension(ob) {
-                // Set height an width of containing elements (li, for now)
-                ob.css({
-                    "height": getDimension(ob).imgHeight,
-                    "width": getDimension(ob).imgWidth
-                });
-                // Set width of overlay (span, for now)
-                ob.find("span").css({
-                    "width": getDimension(ob).imgWidth
-                });
-            }
-            
-            jQuery.fn.setPosition = function() {
-                $(this).css({
-                    "top": getDimension($(this).parent()).imgHeight,
-                    "left": 0
-                });
-            };
-            
-            // Run if all pics are loaded
-            obj.find("img").load(function() {
-                var thisImage = $(this);
-                
-                // Hide overlay content
-                thisImage.siblings("span").css({
-                    "opacity": 0
-                });
-                
-                // Set dimension on container
-                obj.find("li").each(function(index) {
-                    var thisLi = $(this);
-                    setDimension(thisLi);
-                });
-                
-                // Set styles on overlay
-                obj.find("span").css({
-                    "opacity" : 0,
-                    "backgroundColor" : options.overlayColor
-                }).setPosition();
-            });
-            
-            // do the fun stuff
-            // throw in some more effects later
-            obj.find("li").hover(function(event) {
-                $(this).find("span").animate({
-                    opacity: options.overlayTransparency,
-                    top: getDimension($(this)).imgHeight - (getDimension($(this)).spanHeight)
-                }, 200, function() {
-                    // Animation complete.
-                });
-                event.stopPropagation(); // Stop the event bubbling up, no idea if needed twice
-            }, function(event) {
-                $(this).find("span").delay(options.stickDurationAfter).animate({
-                    opacity: 0,
-                    top: getDimension($(this)).imgHeight + (getDimension($(this)).spanHeight)
-                }, 300, function() {
-                    // Animation complete.
-                });
-                event.stopPropagation();
-            });
-        });
-    };
-})(jQuery);
+	$.fn.jPicDetail = function(options) {
 
-$(function(){  
-        $('.jPicDetail').jPicDetail( {
-            stickDurationAfter: 0,
-            overlayColor: "#000",
-            overlayTransparency: 0.8
-         });
-    });
+		var defaults = {
+			stickDurationAfter: 800,
+			overlayColor: "#000",
+			overlayTransparency: 0.8,
+			descriptionTextColor: "#FFF",
+			descriptionPadding: 0
+		};
+		options = $.extend({}, defaults, options);
+
+
+		return this.each(function() {
+			
+			// Define the ul wrapper for easy access
+			var obj = $(this);
+			
+			// Fetch dimension of the image
+			function getDimension(ob) {
+				var dimensions = [];
+				dimensions.imgHeight = ob.find("img").innerHeight();
+				dimensions.imgWidth = ob.find("img").innerWidth();
+				dimensions.spanHeight = ob.find("span").innerHeight();
+				return dimensions;
+			}
+			
+			function setDimension(ob) {
+				// Set height and width of containing elements (li, for now)
+				ob.css({
+					"position": "relative",
+					"overflow": "hidden",
+					"height": getDimension(ob).imgHeight,
+					"width": getDimension(ob).imgWidth
+				});
+				
+				// Set width of overlay (span, for now)
+				ob.find("span").css({
+					"width": getDimension(ob).imgWidth-(options.descriptionPadding*2)
+				});
+			}
+			
+			function setPosition(ob) {
+				// Set Position of description
+				ob.css({
+					"top": getDimension(ob.parent()).imgHeight,
+					"left": 0
+				});
+			}
+			
+			// Run if all pics are loaded
+			obj.find("img").load(function() {
+				
+				// Set dimension on container
+				obj.find("li").each(function(index) {
+					var thisLi = $(this);
+					setDimension(thisLi);
+				});
+				
+				// Set position of description
+				obj.find("li").each(function(index) {
+					var span = $(this).find("img").next("span");
+					setPosition(span);
+				});
+				
+				// Set styles on overlay
+				obj.find("span").css({
+					"opacity": 0,
+					"padding": options.descriptionPadding,
+					"position": "absolute",
+					"backgroundColor": options.overlayColor,
+					"color": options.descriptionTextColor
+				});
+			});
+			
+			// do the fun stuff
+			obj.find("li").hover(function(event) {
+				$(this).find("span").animate({
+					opacity: options.overlayTransparency,
+					top: getDimension($(this)).imgHeight - (getDimension($(this)).spanHeight)
+				}, 200, function() {
+					// Animation complete.
+				});
+				event.stopPropagation(); // Stop the event bubbling up, no idea if needed twice
+			}, function(event) {
+				$(this).find("span").delay(options.stickDurationAfter).animate({
+					opacity: 0,
+					top: getDimension($(this)).imgHeight + (getDimension($(this)).spanHeight)
+				}, 300, function() {
+					// Animation complete.
+				});
+				event.stopPropagation();
+			});
+			
+			// Set description content
+			obj.find("li").each(function(){
+				var img = $(this).find("img");
+				var descr = img.attr("data-descr");
+				img.after("\<span\>"+descr+"\<\/span\>");
+			});
+		});
+	};
+})(jQuery);
